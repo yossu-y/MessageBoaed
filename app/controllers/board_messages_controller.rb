057c9264ee1d4d195e8ec40713470a0bd6e1b40d
yossu-y <yangyuub@gmail.com>
2022-04-22 13:21:55 +0000

    [finish]list5-39

--- app/controllers/board_messages_controller.rb
+++ app/controllers/board_messages_controller.rb
@@ -1,70 +1,64 @@
 class BoardMessagesController < ApplicationController
-  before_action :set_board_message, only: %i[ show edit update destroy ]
+  before_action :set_board_message, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_account!
+  layout 'board_messages'
 
-  # GET /board_messages or /board_messages.json
   def index
-    @board_messages = BoardMessage.all
-  end
-
-  # GET /board_messages/1 or /board_messages/1.json
-  def show
-  end
-
-  # GET /board_messages/new
-  def new
+    @board_messages = BoardMessage.page(params[:page]).order('created_at desc')
+    users = BoardUser.where('account_id == ?', current_account.id)[0]
+    if users == nil then
+      user = BoardUser.new
+      user.account_id = current_account.id
+      user.nickname = '<<no name>>'
+      user.save
+      users = BoardUser.where 'account_id == ?', current_account.id
+    end
+    @board_user = users
     @board_message = BoardMessage.new
+    @board_message.board_user_id = @board_user.id
   end
 
-  # GET /board_messages/1/edit
+  def show
+    redirect_to '/board_messages'
+  end
+
+  def new
+    redirect_to '/board_messages'
+  end
+
   def edit
+    redirect_to '/board_messages'
   end
 
-  # POST /board_messages or /board_messages.json
   def create
+    @board_messages = BoardMessage.page(params[:page]).order('created_at desc')
     @board_message = BoardMessage.new(board_message_params)
-
+    @board_user = BoardUser.where('account_id == ?', current_account.id)[0]
     respond_to do |format|
       if @board_message.save
-        format.html { redirect_to board_message_url(@board_message), notice: "Board message was successfully created." }
+        format.html { redirect_to '/board_messages' }
         format.json { render :show, status: :created, location: @board_message }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :index }
         format.json { render json: @board_message.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /board_messages/1 or /board_messages/1.json
   def update
-    respond_to do |format|
-      if @board_message.update(board_message_params)
-        format.html { redirect_to board_message_url(@board_message), notice: "Board message was successfully updated." }
-        format.json { render :show, status: :ok, location: @board_message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @board_message.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to '/board_messages'
   end
 
-  # DELETE /board_messages/1 or /board_messages/1.json
   def destroy
-    @board_message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to board_messages_url, notice: "Board message was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to '/board_messages'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_board_message
-      @board_message = BoardMessage.find(params[:id])
-    end
+  def set_board_message
+    @board_message = BoardMessage.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def board_message_params
-      params.require(:board_message).permit(:content, :board_user_id)
-    end
+  def board_message_params
+    params.require(:board_message).permit(:content, :board_user_id)
+  end
 end
--- app/controllers/board_users_controller.rb
+++ app/controllers/board_users_controller.rb
@@ -1,70 +1,62 @@
 class BoardUsersController < ApplicationController
-  before_action :set_board_user, only: %i[ show edit update destroy ]
+before_action :set_board_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_account!
+  # layout 'board_users' # 必要に応じて用意
 
-  # GET /board_users or /board_users.json
   def index
-    @board_users = BoardUser.all
-  end
-
-  # GET /board_users/1 or /board_users/1.json
-  def show
-  end
-
-  # GET /board_users/new
-  def new
-    @board_user = BoardUser.new
-  end
-
-  # GET /board_users/1/edit
-  def edit
-  end
-
-  # POST /board_users or /board_users.json
-  def create
-    @board_user = BoardUser.new(board_user_params)
-
-    respond_to do |format|
-      if @board_user.save
-        format.html { redirect_to board_user_url(@board_user), notice: "Board user was successfully created." }
-        format.json { render :show, status: :created, location: @board_user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @board_user.errors, status: :unprocessable_entity }
-      end
+    users = BoardUser.where 'account_id == ?',
+        current_account.id
+    if users[0] == nil then
+      user = BoardUser.new
+      user.account_id = current_account.id
+      user.nickname = '<<no name>>'
+      user.save
+      users = BoardUser.where 'account_id == ?',
+          current_account.id
     end
+    @board_user = users[0]
   end
 
-  # PATCH/PUT /board_users/1 or /board_users/1.json
+  def show
+    @board_user = BoardUser.find params[:id]
+  end
+
+  def new
+    redirect_to '/board_messages'
+  end
+
+  def edit
+    redirect_to '/board_messages'
+  end
+
+  def create
+    redirect_to '/board_messages'
+  end
+
   def update
     respond_to do |format|
       if @board_user.update(board_user_params)
-        format.html { redirect_to board_user_url(@board_user), notice: "Board user was successfully updated." }
-        format.json { render :show, status: :ok, location: @board_user }
+        format.html { redirect_to '/board_messages' }
+        format.json { render :show, status: :ok,
+            location: @board_user }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @board_user.errors, status: :unprocessable_entity }
+        format.html { render :index }
+        format.json { render json: @board_user.errors,
+            status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /board_users/1 or /board_users/1.json
   def destroy
-    @board_user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to board_users_url, notice: "Board user was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to '/board_messages'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_board_user
-      @board_user = BoardUser.find(params[:id])
-    end
+  def set_board_user
+    @board_user = BoardUser.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def board_user_params
-      params.require(:board_user).permit(:nickname, :account_id, :memo)
-    end
+  def board_user_params
+    params.require(:board_user).permit(:nickname, :account_id, :memo)
+  end
 end
